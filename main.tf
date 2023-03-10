c6cd92a2ce06f090e7069c12ea344ff695f893c0
bobklxiao <bobklxiao@tencent.com>
2023-03-10 10:22:53 +0800

    scf module init

---
+++ main.tf
@@ -0,0 +1,27 @@
+# SCF Namespace
+resource "tencentcloud_scf_namespace" "this_namespace" {
+  count           = var.create_namespace ? 1 : 0
+  namespace       = var.namespace_name
+  description     = var.namespace_description
+}
+
+# SCF Function
+resource "tencentcloud_scf_function" "this" {
+  count              = var.create_function ? 1 : 0
+  namespace          = var.function_namespace
+  name               = var.function_name
+  description        = var.function_description
+  zip_file           = var.function_zip_file
+  cos_bucket_name    = var.function_cos_bucket_name == "" ? null : var.function_cos_bucket_name
+  cos_object_name    = var.function_cos_object_name == "" ? null : var.function_cos_object_name
+  cos_bucket_region  = var.function_cos_bucket_region == "" ? null : var.function_cos_bucket_region
+  runtime            = var.function_runtime
+  handler            = var.function_handler
+  mem_size           = var.function_mem_size
+  timeout            = var.function_timeout
+  cls_logset_id      = var.function_cls_logset_id
+  cls_topic_id       = var.function_cls_topic_id
+  vpc_id             = var.function_vpc_id
+  subnet_id          = var.function_subnet_id
+  enable_public_net  = var.function_enable_public_net
+}
